48a50cadbc78bee544bdb0a0ca68e8f5ce23786b
kimkyungsub <cyon256@icloud.com>
2024-09-14 13:20:50 +0900

    functions

--- variable/src/main.rs
+++ variable/src/main.rs
@@ -1,12 +1,8 @@
-use std::io;
 
 fn main() {
-    let nums_data = [1, 2, 3, 4, 5];
-    let mut input = String::new();
-    println!("Input a number");
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-    let input: usize = input.trim().parse().expect("input should be number");
-    println!("{}", nums_data[input])
+    hello_friends();
+}
+
+fn hello_friends(){
+    println!("hello");
 }
