490ee42c1bab49c414cb15fa731e5a486f4d4916
kimkyungsub <cyon256@icloud.com>
2024-09-14 10:12:18 +0900

    data types

--- variable/src/main.rs
+++ variable/src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let nums_data = [1, 2, 3, 4, 5];
+    let mut input = String::new();
+    println!("Input a number");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let input: usize = input.trim().parse().expect("input should be number");
+    println!("{}", nums_data[input])
 }
