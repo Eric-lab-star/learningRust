313f21d7758d1eefa6ee74bcca702ff1d9112bac
kimkyungsub <cyon256@icloud.com>
2024-09-14 22:28:32 +0900

    ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("Hello");
+    s = moving_ownership(s);
+    println!("{s}");
+}
+
+fn moving_ownership(mut s: String) -> String{
+    s.push_str(", world");
+    s
 }
