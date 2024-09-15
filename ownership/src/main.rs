ce7b0a6b247c04637eb771796277a8260e835dce
kimkyungsub <cyon256@icloud.com>
2024-09-15 17:01:42 +0900

    add

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,14 +1,13 @@
 fn main() {
-    let mut s = String::from("Hello");
-    let r1 = &s;
-    let r2 = &s;
-    println!("{r1}, {r2}");
-    println!("{r1}, {r2}");
-
-    let r3 = &mut s;
-    println!("{r3}");
-    
+    let dr = dangling_reference();
 }
+
+fn  dangling_reference() -> String{
+    let s = String::from("hello");
+    s
+}
+
+
 
 
 
