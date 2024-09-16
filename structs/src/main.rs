ac88474af0e415e0610bc47cae7d150a8c6a4b2d
kimkyungsub <cyon256@icloud.com>
2024-09-16 12:30:43 +0900

    method on struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,28 +1,27 @@
-struct User {
-    active: bool,
-    name: String,
-    email: String,
-    sign_in_count: u64,
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn main() {
-    let user = User {
-        active: true,
-        name: String::from("lazy dev"),
-        email: String::from("lazyDev@github.com"),
-        sign_in_count: 100,
-    };
-    println!("{}", user.name);
-    println!("{}", user.active);
-    println!("{}", user.email);
-    println!("{}", user.sign_in_count);
-}
-
-fn build_user(email: String, name: String) -> User {
-    User {
-        email,
-        name,
-        active: true,
-        sign_in_count: 1,
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
     }
 }
+
+
+
+
+fn main() {
+    let rect = Rectangle{
+        width: 12,
+        height: 12,
+    };
+    println!("Area of rect is equal to {}", rect.area());
+}
+
+
+
+
