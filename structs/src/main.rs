1a2b285e6476017d73d4643de4b73d92725bf58f
kimkyungsub <cyon256@icloud.com>
2024-09-16 15:45:36 +0900

    struct method

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,4 +1,3 @@
-
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
@@ -6,22 +5,35 @@ struct Rectangle {
 }
 
 impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        self.width > rect.width && self.height > rect.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
     }
 }
 
-
-
-
 fn main() {
-    let rect = Rectangle{
+    let rect = Rectangle {
         width: 12,
         height: 12,
     };
-    println!("Area of rect is equal to {}", rect.area());
+
+    let sq = Rectangle::square(10);
+    dbg!(&sq);
+
+    let small_rect = Rectangle {
+        width: 10,
+        height: 10,
+    };
+
+    if rect.can_hold(&small_rect) {
+        println!("yes");
+    } else {
+        println!("no");
+    }
 }
-
-
-
-
