cce1fb3727636a282952c3b13a5fca80b71d0b0d
kimkyungsub <cyon256@icloud.com>
2024-09-16 11:11:21 +0900

    struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,28 @@
+struct User {
+    active: bool,
+    name: String,
+    email: String,
+    sign_in_count: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user = User {
+        active: true,
+        name: String::from("lazy dev"),
+        email: String::from("lazyDev@github.com"),
+        sign_in_count: 100,
+    };
+    println!("{}", user.name);
+    println!("{}", user.active);
+    println!("{}", user.email);
+    println!("{}", user.sign_in_count);
+}
+
+fn build_user(email: String, name: String) -> User {
+    User {
+        email,
+        name,
+        active: true,
+        sign_in_count: 1,
+    }
 }
