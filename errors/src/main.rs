1668db046a114e2231bcabf9ff30ee5ec96e5e00
kimkyungsub <cyon256@icloud.com>
2024-09-23 20:18:27 +0900

    errors

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,3 +1,21 @@
+use std::{
+    fs::File,
+    io::{self, Read},
+};
+
 fn main() {
-    println!("Hello, world!");
+    read_file();
+}
+
+fn read_file() -> Result<String, io::Error> {
+    let mut data_file = match File::open("hello.txt") {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+    match data_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
 }
