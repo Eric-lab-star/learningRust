3440fcb2f0452ec4b5b736b366726908345c7662
kimkyungsub <cyon256@icloud.com>
2024-09-25 21:45:24 +0900

    testing

--- lifeTime/src/main.rs
+++ lifeTime/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where 
+    T: Display,
+{
+
+    println!("Announcement! {ann}");
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+
 }
+
+fn main() {}
+
