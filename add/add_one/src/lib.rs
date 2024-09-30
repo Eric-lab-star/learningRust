b96d9d95f9645c47415d178c321595e1c7587367
kimkyungsub <cyon256@icloud.com>
2024-09-30 19:30:56 +0900

    workspace

--- add/add_one/src/lib.rs
+++ add/add_one/src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn add_one(x: i32) -> i32 {
+    x + 1
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn it_works(){
+        assert_eq!(11, add_one(10))
     }
 }
--- add/adder/src/main.rs
+++ add/adder/src/main.rs
@@ -1,3 +1,6 @@
+use add_one;
+
 fn main() {
-    println!("Hello, world!");
+    let num = 10;
+    println!("{}", add_one::add_one(num))
 }
