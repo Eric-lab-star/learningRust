c3368c250328e7636d3adf284b9d7c7c1091dbf3
kimkyungsub <cyon256@icloud.com>
2024-10-06 23:37:36 +0900

    webserver

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,3 +1,11 @@
+use std::sync::Mutex;
+
 fn main() {
-    println!("Hello, world!");
+    let m = Mutex::new(5);
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+
+    println!("m = {m:?}");
 }
