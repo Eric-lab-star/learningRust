e8b41c4af7d207f768a913312580f82904df03a6
kimkyungsub <cyon256@icloud.com>
2024-10-13 16:24:43 +0900

    thread

---
+++ thread/src/lib.rs
@@ -0,0 +1,1 @@
+// lib for lib
--- thread/src/main.rs
+++ thread/src/main.rs
@@ -1,3 +1,12 @@
+use std::{sync::mpsc, thread};
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {received}");
 }
