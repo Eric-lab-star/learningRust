a10c9c0ac441a5996fe616077a7f33111ab940d7
kimkyungsub <cyon256@icloud.com>
2024-10-13 11:03:01 +0900

    first Box type list

--- smart_pointer/src/main.rs
+++ smart_pointer/src/main.rs
@@ -1,14 +1,41 @@
+use core::fmt;
+use std::fmt::{Display, Formatter};
 
-// enum List {
-//     Cons(i32, List),
-//     Nil,
-// }
-//
-//
-// fn main() {
-//     let list = Cons(1, Cons(2, Cons(3, Nil)));
-// }
-//
-fn main() {
-    println!("hello world")
+use crate::List::{Cons, Nil};
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
 }
+
+impl Display for List {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let mut msg: Vec<i32> = vec![];
+        List::walk(self, &mut msg);
+        write!(f, "{:?}", msg)
+    }
+}
+
+impl List {
+    fn walk(list: &List, cup: &mut Vec<i32>){
+        match list {
+            Cons(v, b) => {
+                cup.push(*v);
+                List::walk(&b, cup);
+            }
+            _ => {}
+        } 
+    }
+
+}
+
+
+fn main() {
+    let a_list = Cons(1, Box::new(Nil));
+    let b_list = Cons(2, Box::new(a_list));
+    let c_list = Cons(3, Box::new(b_list));
+
+    println!("{}", c_list);
+}
+
