78c41f9fb44206c49c4b179a0b630514aa7bd6ce
kimkyungsub <cyon256@icloud.com>
2024-11-15 23:39:52 +0900

    reference

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guessing the number");
+    println!("Please input your guess.");
+    let mut guess = String::from("user input");
+    io::stdin()
+        .read_line(&mut guess) // references are immutable by default
+        .expect("Failed to read line");
+    println!("you guessed: {}", guess);
 }
