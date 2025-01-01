dcf719318d9ef9f9998d1887404f5c49497136bc
kimkyungsub <cyon256@icloud.com>
2025-01-02 08:08:13 +0900

    refcell

--- functional/src/main.rs
+++ functional/src/main.rs
@@ -1,5 +1,14 @@
+use std::cell::RefCell;
+
 
 fn main() {
+    let runtime_list = RefCell::new(vec![1,2,3]);
+    let mut iter = runtime_list.borrow_mut();
+    for item in iter.iter_mut() {
+        *item += 1;
+    }
+    println!("{}", iter[0]);
+
 }
 
 
