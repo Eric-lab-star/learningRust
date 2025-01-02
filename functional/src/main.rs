cd5f91c828df2a0db97260fa8151117239d6334c
kimkyungsub <cyon256@icloud.com>
2025-01-02 17:45:42 +0900

    RefCell

--- functional/src/main.rs
+++ functional/src/main.rs
@@ -1,13 +1,30 @@
-use std::cell::RefCell;
+use std::{cell::RefCell, rc::Rc};
 
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            List::Cons(_, item) => Some(item),
+            List::Nil => None,
+        }
+    }
+}
 
 fn main() {
-    let runtime_list = RefCell::new(vec![1,2,3]);
-    let mut iter = runtime_list.borrow_mut();
-    for item in iter.iter_mut() {
-        *item += 1;
-    }
-    println!("{}", iter[0]);
+    let a = Rc::new(List::Cons(5, RefCell::new(Rc::new(List::Nil))));
+    println!("a inital rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(List::Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
 
 }
 
