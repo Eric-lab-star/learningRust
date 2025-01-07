d8231b56c268fc45aa8866e02b6966eedaaff6f6
kimkyungsub <cyon256@icloud.com>
2025-01-08 03:41:56 +0900

    update

---
+++ node/src/main.rs
@@ -0,0 +1,23 @@
+use std::{cell::RefCell, rc::Rc};
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    children: RefCell<Vec<Rc<Node>>>
+}
+
+
+fn main() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        children: RefCell::new(vec![]),
+    });
+
+    let branch = Rc::new(Node {
+        value: 5,
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    })
+
+    
+}
+
