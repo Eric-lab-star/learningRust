e91bb50c531f7a5595a30a3c7579270a9d4472e3
kimkyungsub <cyon256@icloud.com>
2025-01-20 08:29:12 +0900

    oop pattern

---
+++ trait_object/src/main.rs
@@ -0,0 +1,31 @@
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    pub fn draw(&self) {
+        println!("drawn button");
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
